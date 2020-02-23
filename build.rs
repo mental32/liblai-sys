863253c4d0f3c308f04677dbb8cc574f5d64f1c8
mental <m3nta1@yahoo.com>
2020-02-23 16:35:59 +0000

    Expose all of the public lai api

--- build.rs
+++ build.rs
@@ -4,15 +4,30 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-
     let bindings = bindgen::Builder::default()
         // Use core instead of libstd in the generated bindings.
         .use_core()
+        // Detect include paths
+        .detect_include_paths(true)
         // Use libc as the ctypes prefix instead of libstd.
         .ctypes_prefix("libc")
-        // The input header we would like to generate
-        // bindings for.
+        // Include path
+        .clang_arg("-I./lai/include")
+        // Avoid deriving debug since a lot of the structs are packed
+        .derive_debug(false)
+        // Header files.
         .header("./lai/include/lai/host.h")
+        .header("./lai/include/lai/helpers/resource.h")
+        .header("./lai/include/acpispec/resources.h")
+        .header("./lai/include/acpispec/hw.h")
+        .header("./lai/include/acpispec/tables.h")
+        .header("./lai/include/lai/core.h")
+        .header("./lai/include/lai/drivers/ec.h")
+        .header("./lai/include/lai/drivers/timer.h")
+        .header("./lai/include/lai/helpers/pc-bios.h")
+        .header("./lai/include/lai/helpers/pci.h")
+        .header("./lai/include/lai/helpers/pm.h")
+        .header("./lai/include/lai/helpers/sci.h")
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
